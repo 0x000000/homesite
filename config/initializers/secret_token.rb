937118621136a51b8a42b059ff3d823d7eb2c269
Alexander Rudenko <mur.mailbox@gmail.com>
2011-10-27 22:51:06 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsite::Application.config.secret_token = '849dc29f1a9acecae06a8fb04e0a47a7d2118f55c5d99585d10de5aa13f4ae688f3d88f0a6c9668dc50d510c9d0a0e4779ed629d8f1cd8597476bec4eb75ce65'
