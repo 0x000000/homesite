9f1ebdab23d7a52991de2e4a4d2c9e73b6da6949
Alexander Rudenko <mur.mailbox@gmail.com>
2011-11-02 17:51:12 -0700

    Added streamline to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @stream = Entry.for_stream
   end
 
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+  def sort_desc some_hash
+    some_hash.keys.sort.reverse.each do |key|
+      yield key, some_hash[key]
+    end
+  end
+
+  alias :stream_years_by :sort_desc
+  alias :stream_months_by :sort_desc
+  alias :stream_days_by :sort_desc
 end
